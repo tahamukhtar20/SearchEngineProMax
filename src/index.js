b50a923712004c9e1486cf7c62b6bf0706c02c61
Muhammad Sunaam <msunaam2002@gmail.com>
2024-05-14 09:29:18 +0500

    update

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ const BloomFilter = require("./BloomFilter");
 // debugger;
 
 // const bFilter = new BloomFilter();
-const crawler = new Crawler();
+const crawler = new Crawler();In case of running it in PySpark shell via pyspark executable, the shell autIn case of running it in PySpark shell via pyspark executable, the shell aut
 
 crawler.start();
